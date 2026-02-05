87e2b8775b0ab113aa2518b896fbfc3ccadac0e3
Pavelwell7 <pavelbugaev303@gmail.com>
2026-02-05 23:39:03 +0300

    Add files via upload

---
+++ property/migrations/0011_auto_20260204_1355.py
@@ -0,0 +1,29 @@
+import phonenumbers
+
+from django.db import migrations
+
+def normalize_number(apps, schema_editor):
+    Flat = apps.get_model('property', 'Flat')
+    for flat in Flat.objects.all().iterator():
+        raw_number = flat.phone_number
+        if raw_number:
+            parsed_number = phonenumbers.parse(raw_number, 'RU')
+            if phonenumbers.is_valid_number(parsed_number):
+                flat.pure_phone = parsed_number
+                flat.save()
+
+
+def move_backend(apps, schema_editor):
+    Flat = apps.get_model('property', 'Flat')
+    Flat.objects.all().update(owner_pure_phone='')
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('property', '0010_alter_flat_owner_pure_phone'),
+    ]
+
+    operations = [
+        migrations.RunPython(normalize_number, move_backend)
+    ]
